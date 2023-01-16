9461eb5e375c5b5a0ff025bd98119e2ac0774ae9
Abhishek-90 <holaniabhishek90@gmail.com>
2023-01-16 20:19:16 +0530

    added constants

--- Shared/Constants/EnvConstants.ts
+++ Shared/Constants/EnvConstants.ts
@@ -4,3 +4,7 @@ dotenv.config({ path: ".env" });
 
 export const PORT = process.env.port;
 export const API_KEY = process.env.YOUTUBE_API_KEY;
+
+// Youtube Endpoint constants
+export const GET_VIDEOS_ENDPOINT = process.env.YOUTUBE_GET_VIDEOS_ENDPOINT;
+export const GET_CHANNEL_ENDPOINT = process.env.YOUTUBE_GET_CHANNEL_ENDPOINT;
