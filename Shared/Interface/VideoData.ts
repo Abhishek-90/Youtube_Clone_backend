752d2a66366e5f31564a08a92f09d110db2176bc
Abhishek-90 <holaniabhishek90@gmail.com>
2023-01-16 19:50:05 +0530

    added interface

---
+++ Shared/Interface/VideoData.ts
@@ -0,0 +1,13 @@
+export interface IVideoData {
+  id: string;
+  thumbnailUrl: string;
+  viewCount: number;
+  likeCount: number;
+  commentCount: number;
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  channelTitle: string;
+  channelThumbnailUrl: string;
+}
