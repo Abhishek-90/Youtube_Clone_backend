fee4a6e12d99905dee6b7d3ac586655597e8702d
Abhishek-90 <holaniabhishek90@gmail.com>
2023-01-30 10:24:21 +0530

    seperated video fetching code.

---
+++ Controller/Functions/Videos.ts
@@ -0,0 +1,47 @@
+import { IVideoData } from "../../Shared/Interface/VideoData";
+import * as C from "../../Shared/Constants/EnvConstants";
+import * as HS from "../../Shared/Constants/HTTPStatus";
+
+export const getVideosFromAPI = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    const videoItems = json.items;
+
+    let videoData: IVideoData[] = videoItems.map((video: any) => {
+      // Retrieveing Videos Data.
+      return {
+        id: video.id,
+        title: video.snippet.title,
+        description: video.snippet.description,
+        publishedAt: video.snippet.publishedAt,
+        thumbnailUrl:
+          video.snippet.thumbnails.maxres?.url ||
+          video.snippet.thumbnails.high?.url ||
+          video.snippet.thumbnails.medium?.url,
+        viewCount: video.statistics.viewCount,
+        likeCount: video.statistics.likeCount,
+        commentCount: video.statistics.commentCount,
+        channelTitle: video.snippet.channelTitle,
+        channelId: video.snippet.channelId,
+        channelThumbnailUrl: "",
+      };
+    });
+
+    for (const video of videoData) {
+      const channelResponse = await fetch(
+        `${C.GET_CHANNEL_ENDPOINT}id=${video.channelId}&key=${C.API_KEY}&part=snippet`,
+        {
+          method: "GET",
+        }
+      );
+      const channelJson = await channelResponse.json();
+      video.channelThumbnailUrl =
+        channelJson.items[0].snippet.thumbnails.high.url;
+    }
+
+    return { status: HS.BADREQUEST, videoData };
+  } catch (error) {
+    return { status: HS.BADREQUEST, error };
+  }
+};
--- Controller/Videos.ts
+++ Controller/Videos.ts
@@ -1,52 +1,24 @@
 import express, { Request, Response } from "express";
 import * as C from "../Shared/Constants/EnvConstants";
-import { IVideoData } from "../Shared/Interface/VideoData";
+import { getVideosFromAPI } from "./Functions/Videos";
 import * as HS from "../Shared/Constants/HTTPStatus";
 
 const router = express.Router();
+
 router.get("/getVideos", async (req: Request, res: Response) => {
-  try {
-    const response = await fetch(
-      `${C.GET_VIDEOS_ENDPOINT}part=player,statistics,snippet&key=${C.API_KEY}&chart=mostPopular&maxResults=20`
-    );
-    const json = await response.json();
-    const videoItems = json.items;
+  const url: string = `${C.GET_VIDEOS_ENDPOINT}part=player,statistics,snippet&key=${C.API_KEY}&chart=mostPopular&maxResults=20`;
+  const responseFromService = await getVideosFromAPI(url);
 
-    let videoData: IVideoData[] = videoItems.map((video: any) => {
-      // Retrieveing Videos Data.
-      return {
-        id: video.id,
-        title: video.snippet.title,
-        description: video.snippet.description,
-        publishedAt: video.snippet.publishedAt,
-        thumbnailUrl:
-          video.snippet.thumbnails.maxres?.url ||
-          video.snippet.thumbnails.high?.url ||
-          video.snippet.thumbnails.medium?.url,
-        viewCount: video.statistics.viewCount,
-        likeCount: video.statistics.likeCount,
-        commentCount: video.statistics.commentCount,
-        channelTitle: video.snippet.channelTitle,
-        channelId: video.snippet.channelId,
-        channelThumbnailUrl: "",
-      };
-    });
-
-    for (const video of videoData) {
-      const channelResponse = await fetch(
-        `${C.GET_CHANNEL_ENDPOINT}id=${video.channelId}&key=${C.API_KEY}&part=snippet`,
-        {
-          method: "GET",
-        }
-      );
-      const channelJson = await channelResponse.json();
-      video.channelThumbnailUrl =
-        channelJson.items[0].snippet.thumbnails.high.url;
-    }
-
-    return res.status(HS.OK).json({ videoData }).end();
-  } catch (error) {
-    return res.status(HS.BADREQUEST).json({ error });
+  if (responseFromService.status === HS.OK) {
+    return res
+      .status(responseFromService.status)
+      .json({ videoData: responseFromService.videoData })
+      .end();
+  } else if (responseFromService.status === HS.BADREQUEST) {
+    return res
+      .status(responseFromService.status)
+      .json({ error: responseFromService.error })
+      .end();
   }
 });
 
