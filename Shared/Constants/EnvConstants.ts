fd27c444b0d9ed74211a51eb3f4d686ffc8cef33
Abhishek-90 <holaniabhishek90@gmail.com>
2023-02-02 12:57:40 +0530

    new endpoint added

--- Shared/Constants/EnvConstants.ts
+++ Shared/Constants/EnvConstants.ts
@@ -6,6 +6,7 @@ export const PORT = process.env.port;
 export const API_KEY = process.env.YOUTUBE_API_KEY;
 
 // Youtube Endpoint constants
+export const YOUTUBE_API = process.env.YOUTUBE_API;
 export const GET_VIDEOS_ENDPOINT = process.env.YOUTUBE_GET_VIDEOS_ENDPOINT;
 export const GET_CHANNEL_ENDPOINT = process.env.YOUTUBE_GET_CHANNEL_ENDPOINT;
 export const GET_SEARCH_VIDEOS_ENDPOINT =
